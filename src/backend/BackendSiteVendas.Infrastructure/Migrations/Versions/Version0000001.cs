9acea98480ef91632199ed387d509e625410082e
cristian do santos <cristiandosantos98@outlook.com>
2023-05-15 15:42:49 -0300

    Adicionando as classes de migration da tabela usuario

---
+++ src/backend/BackendSiteVendas.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace BackendSiteVendas.Infrastructure.Migrations.Versions;
+    
+[Migration((long)VersionsNumber.CriarTabelaUsuario, "Cria tabela Usuario")]
+public class Version0000001 : Migration
+{
+    public override void Down()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Up()
+    {
+        var table = VersionBase.InsertDefaultColumns(Create.Table("Usuario"));
+
+        table
+            .WithColumn("Nome").AsString(100).NotNullable()
+            .WithColumn("Email").AsString(100).NotNullable()
+            .WithColumn("Senha").AsString(2000).NotNullable()
+            .WithColumn("Telefone").AsString(14).NotNullable();
+    }
+}
