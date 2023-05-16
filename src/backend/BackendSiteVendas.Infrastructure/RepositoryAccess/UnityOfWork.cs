aa12688c78105fff947fde13e45421f9160c56f5
cristian do santos <cristiandosantos98@outlook.com>
2023-05-16 10:20:53 -0300

    ✨: Classes para acessar o repositorio

---
+++ src/backend/BackendSiteVendas.Infrastructure/RepositoryAccess/UnityOfWork.cs
@@ -0,0 +1,35 @@
+﻿using BackendSiteVendas.Domain.Repositories;
+
+namespace BackendSiteVendas.Infrastructure.RepositoryAccess;
+
+public sealed class UnityOfWork : IDisposable, IUnityOfWork
+{
+    private readonly BackendSiteVendasContext _context;
+    private bool _disposed;
+
+    public UnityOfWork(BackendSiteVendasContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Commit()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (!_disposed && disposing)
+        {
+            _context.Dispose();
+        }
+
+        _disposed = true;
+    }
+
+}
