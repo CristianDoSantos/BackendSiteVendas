76f300274bac10081295aa285dc2063b4ef74cce
cristian do santos <cristiandosantos98@outlook.com>
2023-05-18 15:32:38 -0300

    ✨: Adicionado testes de unidade para registrar usuario

---
+++ tests/UtilitiesForTests/Mapper/MapperBuilder.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BackendSiteVendas.Application.Services.AutoMapper;
+
+namespace UtilitiesForTests.Mapper;
+
+public class MapperBuilder
+{
+    public static IMapper Instance()
+    {
+        var configuration = new MapperConfiguration(cfg => {
+            cfg.AddProfile<AutoMapperConfiguration>();
+        });
+
+        return configuration.CreateMapper();
+    }
+}
---
+++ tests/UtilitiesForTests/Repositories/UserWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,29 @@
+﻿using BackendSiteVendas.Domain.Repositories;
+using Moq;
+
+namespace UtilitiesForTests.Repositories;
+
+public class UserWriteOnlyRepositoryBuilder
+{
+    private static UserWriteOnlyRepositoryBuilder _instace;
+    private readonly Mock<IUserWriteOnlyRepository> _repository;
+
+    private UserWriteOnlyRepositoryBuilder()
+    {
+        if (_repository is null)
+        {
+            _repository= new Mock<IUserWriteOnlyRepository>();
+        }
+    }
+
+    public static UserWriteOnlyRepositoryBuilder Instance()
+    {
+        _instace= new UserWriteOnlyRepositoryBuilder();
+        return _instace;
+    }
+
+    public IUserWriteOnlyRepository Build()
+    {
+        return _repository.Object;
+    }
+}
