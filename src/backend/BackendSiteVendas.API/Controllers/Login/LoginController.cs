83221cd44a2e7e72f5efdc19bdb86c627407fbf7
cristian do santos <cristiandosantos98@outlook.com>
2023-05-19 11:39:35 -0300

    ✨: Adicionado endpoint para realizar o login e atualizado o postmanCollection com o novo endpoint

---
+++ src/backend/BackendSiteVendas.API/Controllers/Login/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using BackendSiteVendas.Application.UseCases.Login.DoLogin;
+using BackendSiteVendas.Comunication.Requests;
+using BackendSiteVendas.Comunication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendSiteVendas.API.Controllers.Login
+{
+    public class LoginController : BackendSiteVendasController
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(LoginResponseJson), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Login(
+           [FromServices] ILoginUseCase useCase,
+           [FromBody] LoginRequestJson request)
+        {
+            var response = await useCase.Execute(request);
+
+            return Ok(response);
+        }
+    }
+}
