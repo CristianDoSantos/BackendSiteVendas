7f3926b76fc8fff4751c62d0c4db68f47dec8cb5
cristian do santos <cristiandosantos98@outlook.com>
2023-05-21 21:57:11 -0300

    ✨: adicionado testes unitarios para a validação da requisiçao de trocar a senha

---
+++ tests/UtilitiesForTests/Requests/ChangePasswordRequestBuilder.cs
@@ -0,0 +1,14 @@
+﻿using BackendSiteVendas.Comunication.Requests;
+using Bogus;
+
+namespace UtilitiesForTests.Requests;
+
+public class ChangePasswordRequestBuilder
+{
+    public static ChangePasswordRequestJson Build(int passwordLength = 10)
+    {
+        return new Faker<ChangePasswordRequestJson>()
+            .RuleFor(request => request.CurrentPassword, f => f.Internet.Password(10))
+            .RuleFor(request => request.NewPassword, f => f.Internet.Password(passwordLength));
+    }
+}
